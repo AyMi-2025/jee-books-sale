a7bc2a8a3c6caad04826cd8820fc5a1ea0bb089c
AyMi-2025 <ayanmaiti558@gmail.com>
2026-02-05 19:34:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ function createBookCard(book) {
 
 // Open WhatsApp with pre-filled message
 function openWhatsApp(book) {
-    const message = `Hi! I'm interested in buying "${book.title}" by ${book.author} for ₹${book.sellingPrice}`;
+    const message = `Hi! I'm interested in buying "${book.title}" by ${book.author}`;
     const url = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
 }
